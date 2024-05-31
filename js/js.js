47ef2d4df5076db293a9881a8b0c5e23bfc7c29b
Politikar <85663218+Politikar@users.noreply.github.com>
2024-05-31 22:48:03 +0900

    エラー

--- js/js.js
+++ js/js.js
@@ -1,3 +1,14 @@
+function sCO2() {
+  var s1 = document.getElementById('s-consumption');
+  var scon = Number(s1.value);
+  var s2 = document.getElementById('co2-s-coefficient');
+  var seff = Number(s2.value);
+  var s_sub = (scon / seff)*0.8;
+  var s = Math.round(s_sub);
+  document.getElementById('co2-emission-s').textContent = s.toFixed(2);
+}
+
+
 function roundToDecimalPlace(number) {
   var s1 = document.getElementById('s-consumption');
   var scon = Number(s1.value);
@@ -6,14 +17,4 @@ function roundToDecimalPlace(number) {
   var number = (scon / seff)*0.8;
   var s = Math.round(number * Math.pow(10, 1)) / Math.pow(10, 1);
   document.getElementById('emission-s').textContent = s.toFixed(2);
-}
-
-function sCO2() {
-  var s1 = document.getElementById('s-consumption');
-  var scon = Number(s1.value);
-  var s2 = document.getElementById('co2-s-coefficient');
-  var seff = Number(s2.value);
-  var s_sub = (scon / seff)*0.8;
-  var s = Math.round(s_sub, 1);
-  document.getElementById('co2-emission-s').textContent = s.toFixed(2);
 }
