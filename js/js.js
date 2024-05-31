adfcaf2eb61029f91fc7cf356962e5b6f5a7f583
Politikar <85663218+Politikar@users.noreply.github.com>
2024-05-31 22:28:54 +0900

    四捨五入

--- js/js.js
+++ js/js.js
@@ -1,4 +1,8 @@
-// 水道からのCO2を計算する。
+function roundToDecimalPlace(number, decimalPlaces) {
+  const factor = Math.pow(10, decimalPlaces);
+  return Math.round(number * factor) / factor;
+}
+
 function sCO2() {
   var s1 = document.getElementById('s-consumption');
   var scon = Number(s1.value);
@@ -6,7 +10,7 @@ function sCO2() {
   var s2 = document.getElementById('co2-s-coefficient');
   var seff = Number(s2.value);
   var s_sub = (scon / seff)*0.8;
-  var s = Math.round(s_sub);
+  var s = roundToDecimalPlace(s_sub, 1);
 
   document.getElementById('co2-emission-s').textContent = s.toFixed(2);
 }
