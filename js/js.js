8016a40bdf8186661c2541c8fc1bf7ddee19b7ae
Politikar <85663218+Politikar@users.noreply.github.com>
2024-05-31 22:43:21 +0900

    四捨五入

--- js/js.js
+++ js/js.js
@@ -1,12 +1,11 @@
 function roundToDecimalPlace(number) {
-  const factor = Math.pow(10, 1);
   var s1 = document.getElementById('s-consumption');
   var scon = Number(s1.value);
   var s2 = document.getElementById('co2-s-coefficient');
   var seff = Number(s2.value);
   var number = (scon / seff)*0.8;
-  var s = Math.round(number * factor) / factor;
-  document.getElementById('co2-emission-s').textContent = s.toFixed(2);
+  var s = Math.round(number * Math.pow(10, 1)) / Math.pow(10, 1);
+  document.getElementById('emission-s').textContent = s.toFixed(2);
 }
 
 function sCO2() {
