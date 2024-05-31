556b8c1f807850de8629e520692d19bac5a861a2
Politikar <85663218+Politikar@users.noreply.github.com>
2024-05-31 22:25:19 +0900

    微調整

--- js/js.js
+++ js/js.js
@@ -5,7 +5,7 @@ function sCO2() {
 
   var s2 = document.getElementById('co2-s-coefficient');
   var seff = Number(s2.value);
-  var s_sub = (scon / seff)*0.8
+  var s_sub = (scon / seff)*0.8;
   var s = Math.round(s_sub);
 
   document.getElementById('co2-emission-s').textContent = s.toFixed(2);
