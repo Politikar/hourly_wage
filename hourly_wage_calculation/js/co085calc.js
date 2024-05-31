55a14b322c192258012f36fb23e2e91c883ce845
Politikar <85663218+Politikar@users.noreply.github.com>
2024-05-31 21:58:58 +0900

    Add files via upload

---
+++ hourly_wage_calculation/js/co085calc.js
@@ -0,0 +1,12 @@
+// 水道からのCO2を計算する。
+function sCO2() {
+  var s1 = document.getElementById('s-consumption');
+  var scon = Number(s1.value);
+
+  var s2 = document.getElementById('co2-s-coefficient');
+  var seff = Number(s2.value);
+
+  var s = (scon / seff)*0.8;
+
+  document.getElementById('co2-emission-s').textContent = s.toFixed(2);
+}
